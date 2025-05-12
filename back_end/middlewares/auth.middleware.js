92473560fc5bcbea8e7d1e8490983e99b90a5ec0
Farrux Temurov <tfarrux70@gmail.com>
2025-05-12 12:46:34 +0500

    auth middleware added

---
+++ back_end/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+    console.log("authMiddleware called...");
+
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Token required for access" });
+    }
+
+    try {
+        const decoded = await jwt.verify(token, process.env.MY_TOKEN);
+        req.user = decoded;        // Save decoded user info (email, role)
+        req.role = decoded.role;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid or expired token", error: err.message });
+    }
+};
+
+const adminOnly = async (req, res, next) => {
+    if (req.role === "admin") {
+        return next();
+    }
+    return res.status(403).json({ message: "You are not authorized to access this endpoint" });
+};
+
+module.exports = {
+    authMiddleware,
+    adminOnly
+};
