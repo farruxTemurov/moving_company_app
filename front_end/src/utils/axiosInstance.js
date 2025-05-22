5caf757862524aa333c2b20f2c9d6643b2fa4f73
Farrux Temurov <tfarrux70@gmail.com>
2025-05-22 22:34:52 +0500

    updated axiosInstance

--- front_end/src/utils/axiosInstance.js
+++ front_end/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VVITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Add a request interceptor to include the token in headers
