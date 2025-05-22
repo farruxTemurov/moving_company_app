a5fbb1c18ece1adbb81e0fa2d98667c9a1fa9eb5
Farrux Temurov <tfarrux70@gmail.com>
2025-05-22 22:34:15 +0500

    updated app.js

--- back_end/app.js
+++ back_end/app.js
@@ -26,8 +26,13 @@ app.use("/api/bookings", bookingsRoute.router);
 // General error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({ message: "Internal Server Error", error: err.message });
+
+    const statusCode = err.status || 500;
+    res.status(statusCode).json({
+        message: err.message || "Internal Server Error"
+    });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
